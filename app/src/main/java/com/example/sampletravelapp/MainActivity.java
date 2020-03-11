c8db87efdb7fd963f55c99b5e580ce3908348c39
Malathi <57785954+Malathi1993@users.noreply.github.com>
2020-03-11 17:33:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sampletravelapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.sampletravelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+ImageView image;
+TextView Travelname;
+TextView Travelname1;
+Button Signup;
+Button Signin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image=(ImageView) findViewById(R.id.imageView2);
+        Travelname=(TextView) findViewById(R.id.tv_travelname);
+        Travelname1=(TextView) findViewById(R.id.tv_travelname1);
+        Signup=(Button) findViewById(R.id.btn_signup);
+        Signin=(Button) findViewById(R.id.btn_signin);
+
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/sampletravelapp/SignInActivity.java
@@ -0,0 +1,25 @@
+package com.example.sampletravelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignInActivity extends AppCompatActivity {
+Button SignIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        SignIn=(Button)findViewById(R.id.btn_signup);
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
