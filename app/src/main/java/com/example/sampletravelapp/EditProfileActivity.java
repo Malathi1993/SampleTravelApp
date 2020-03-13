780533dad63ea0d108830693f279510dd9843574
Malathi <57785954+Malathi1993@users.noreply.github.com>
2020-03-13 19:23:10 +0530

    add

--- app/src/main/java/com/example/sampletravelapp/EditProfileActivity.java
+++ app/src/main/java/com/example/sampletravelapp/EditProfileActivity.java
@@ -2,13 +2,26 @@ package com.example.sampletravelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class EditProfileActivity extends AppCompatActivity {
+    TextView MadhuriTravels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        MadhuriTravels = (TextView) findViewById(R.id.textView6);
+        MadhuriTravels.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditProfileActivity.this, ToursActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sampletravelapp/InformationActivity.java
+++ app/src/main/java/com/example/sampletravelapp/InformationActivity.java
@@ -2,13 +2,25 @@ package com.example.sampletravelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class InformationActivity extends AppCompatActivity {
-
+TextView MadhuriTravels;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        MadhuriTravels=(TextView)findViewById(R.id.textView9);
+        MadhuriTravels.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InformationActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
